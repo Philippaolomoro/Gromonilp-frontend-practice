1ce82255ac6348dbf752fdaab81f2a7b15375aba
Philippa Olomoro <olomoro.philippa@gmail.com>
2020-07-17 17:37:01 +0100

    added the header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+	render() {
+		return (
+			<div className="text-center header">
+				<h1 className="text-white header-h1">GROMONI</h1>
+			</div>
+		);
+	}
+}
+
+export default Header;
